0e5de958301cda0563325e30e3d994e9596818fa
Adarsh Mishra <68419912+MeAdarshMishra@users.noreply.github.com>
2020-08-17 00:12:39 +0530

    Add files via upload

---
+++ sumlist.py
@@ -0,0 +1,11 @@
+N=[11,5,10,20,23]
+x=int(input("Enter a Number to know sum is present or not"))
+m=int()
+for i in range(0,len(N)):
+    m=m+N[i]
+if(x==n):
+    print("Yes")
+else:
+    print("No")
+    
+
